a6050a2f46a71186c918d3626109f3eade9d9e70
mobile-build-persona <mobile-build@withpersona.com>
2025-10-07 13:25:49 -0600

    Update Package.swift for 2.30.4

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "PersonaSentinel",
-      url: "https://github.com/persona-id/sentinel-ios/releases/download/2.30.3/PersonaSentinel.xcframework.zip",
-      checksum: "fc4a455be9a46be63df748d4ae93e4f04bb410d636a93211b0181ae1f77b3684"
+      url: "https://github.com/persona-id/sentinel-ios/releases/download/2.30.4/PersonaSentinel.xcframework.zip",
+      checksum: "2f586c03cb9d8a93a3f79e4b2ad484015d46118e108fd93b750a058771cc58bb"
     )
   ]
 )
