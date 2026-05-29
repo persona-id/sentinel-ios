9b468ff01482e9819446bf09702ad2876fe49d10
mobile-build-persona <mobile-build@withpersona.com>
2026-05-28 18:45:43 -0600

    Bump to 2.48.1-beta.2

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "PersonaSentinel",
-      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/2.48.1-beta.1/PersonaSentinel.xcframework.zip",
-      checksum: "6e36bb9e0b240685a6c51c18a77a1cc2e47e891837204c5d53c096b71610c9fb"
+      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/2.48.1-beta.2/PersonaSentinel.xcframework.zip",
+      checksum: "1234af2918644c3bcdde661b0c1dc1ba367cf117cde02761b3ebcf1cfb239447"
     )
   ]
 )
